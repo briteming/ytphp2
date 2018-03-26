bf869af30f905fefdec0ce815e35c2effd962961
Devilsking <leonguyen52@outlook.com>
2018-03-26 14:43:29 +0800

    Update config.php

--- web/config.php
+++ web/config.php
@@ -1,9 +1,9 @@
 <?php
 define('ROOT_PART', Root_part());
-define('APIKEY', 'AIzaSyAiWoHNeDOg9wfPn20ZqoFcYdSRMNZXHbg');
-define('GJ_CODE', 'VN');
-define('SITE_NAME', 'LeoCloud YT');
-define('TITLENAME', 'LeoCloud YT');
-define('EN2DEKEY', 'Z!-j^TN|WJ-+EXB|G-8V3dP+r*<OAjB07zBD@VAbk?@(oT%)D+.^RqbA@O|(Tn#');
-define('EMAIL', 'admin@leocloud.uk');
+define('APIKEY', getenv("APIKEY"));
+define('GJ_CODE', getenv("GJ_CODE"));
+define('SITE_NAME',getenv("SITE_NAME"));
+define('TITLENAME', getenv("TITLENAME"));
+define('EN2DEKEY',getenv("EN2DEKEY"));
+define('EMAIL', getenv("EMAIL"));
 ?>
